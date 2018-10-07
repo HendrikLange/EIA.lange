19a3bf397d14673dd7ac13e169fea1eadf037900
Hendrixen <Hendrixen@DESKTOP-1Q96687>
2018-10-07 17:40:57 +0200

    Ayo

--- EIA2/Aufgabe0/aufgabe0.ts
+++ EIA2/Aufgabe0/aufgabe0.ts
@@ -2,12 +2,12 @@ namespace aufgabe0 {
     var i : string ="";
         
     function main() {
-        var i = prompt("Wie heisst du?");
+        var i = prompt("Wie lautet dein Name?");
         var node : any = document.getElementById("body");
         node.innerHTML += "Yo ";
         node.innerHTML += i;
-        node.innerHTML += ", Was geht ab.";
-        console.log("Yo ",i,", Was Geht ab.");
+        node.innerHTML += ", was geht ab.";
+        console.log("Yo ",i,", as Geht ab.");
         }
 
     
